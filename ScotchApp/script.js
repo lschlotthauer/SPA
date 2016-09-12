47a3710e60d5dffd2e985e0498cc7a477ee1ced6
lschlotthauer <lucas6sic6@gmail.com>
2016-09-12 13:46:31 -0300

    Commit inicial

---
+++ ScotchApp/script.js
@@ -0,0 +1,60 @@
+// script.js
+
+    // create the module and name it scotchApp
+        // also include ngRoute for all our routing needs
+    var scotchApp = angular.module('scotchApp', ['ngRoute']);
+
+    // configure our routes
+    scotchApp.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'pages/home.html',
+                controller  : 'mainController'
+            })
+
+            // route for the about page
+            .when('/about', {
+                templateUrl : 'pages/about.html',
+                controller  : 'aboutController'
+            })
+
+            // route for the contact page
+            .when('/contact', {
+                templateUrl : 'pages/contact.html',
+                controller  : 'contactController'
+            });
+    });
+
+    // create the controller and inject Angular's $scope
+    scotchApp.controller('mainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
+
+    scotchApp.controller('aboutController', function($scope) {
+        $scope.message = 'Look! I am an about page.';
+    });
+
+    scotchApp.controller('contactController', function($scope) {
+        $scope.message = 'Contact us! JK. This is just a demo.';
+    });
+
+function getRss(){
+   $(function(){
+        url = 'http://rss.cnn.com/rss/edition.rss';
+        $.ajax({
+        type: "GET",
+        url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url),
+        dataType: 'json',
+        error: function(){
+            alert('Unable to load feed, Incorrect path or invalid feed');
+        },
+        success: function(xml){
+            values = xml.responseData.feed.entries;
+            console.log(values);
+        }
+    });
+    });
+}
