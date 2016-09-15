9a7b4b51cc68361ef38ac615cebca178b8d236e2
lschlotthauer <lucas6sic6@gmail.com>
2016-09-15 10:15:39 -0300

    Se agrego proyecto desde Original

---
+++ CodeCamper.Web/CodeCamper.Model/Session.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace CodeCamper.Model
+{
+    public class Session : SessionBrief
+    {
+        public string Description { get; set; }
+        public Person Speaker { get; set; }
+        public virtual Track Track { get; set; }
+        public virtual TimeSlot TimeSlot { get; set; }
+        public virtual Room Room { get; set; }
+
+        public virtual ICollection<Attendance>
+            AttendanceList { get; set; }
+
+    }
+}
