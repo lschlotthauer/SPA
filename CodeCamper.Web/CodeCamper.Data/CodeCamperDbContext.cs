73edbe3fd3d99d02fabd86729b357d224636e3af
lschlotthauer <lucas6sic6@gmail.com>
2016-09-15 16:15:50 -0300

    DBContext

---
+++ CodeCamper.Web/CodeCamper.Data/CodeCamperDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeCamper.Data.SampleData;
+using CodeCamper.Model;
+
+namespace CodeCamper.Data
+{
+    public class CodeCamperDbContext : DbContext
+    {
+        public CodeCamperDbContext()
+            : base(nameOrConnectionString: "CodeCamper") { }
+
+        public DbSet<Session> Sessions { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Attendance> Attendance { get; set; }
+        
+        // Lookup Lists
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<TimeSlot> TimeSlots { get; set; }
+        public DbSet<Track> Tracks { get; set; }
+
+        static CodeCamperDbContext()
+        {
+            Database.SetInitializer(new CodeCamperDatabaseInitializer());
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Configurations.Add(new SessionConfiguration());
+            modelBuilder.Configurations.Add(new AttendanceConfiguration());
+        }
+    }
+}
