1e880d72e07eecc3212f5ff1e35f0cc729c6d086
lschlotthauer <lucas6sic6@gmail.com>
2016-09-21 15:01:41 -0300

    repository

---
+++ CodeCamper.Web/CodeCamper.Data.Contracts/ICodeCamperUow.cs
@@ -0,0 +1,17 @@
+﻿using CodeCamper.Model;
+
+namespace CodeCamper.Data.Contracts
+{
+    public interface ICodeCamperUow
+    {
+        // Save pending changes to the data store
+        void Commit();
+
+        // Repositories
+        IPersonsRepository Persons { get; }
+        IRepository<Room> Rooms { get; }
+        ISessionsRepository Sessions { get; }
+        IRepository<TimeSlot> TimeSlots { get; }
+        IAttendanceRepository Attendance { get; }
+    }
+}
---
+++ CodeCamper.Web/CodeCamper.Data.Contracts/IPersonsRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeCamper.Model;
+
+namespace CodeCamper.Data.Contracts
+{
+    public interface IPersonsRepository : IRepository<Person>
+    {
+        /// <summary>
+        /// Get <see cref="Speaker"/>s at sessions.
+        /// </summary>
+        /// <remarks>
+        /// <see cref="Speaker"/> is a  subset of
+        /// <see cref="Person"/> properties suitable for
+        /// quick client-side filtering and presentation.
+        /// </remarks>
+        IQueryable<Speaker> GetSpeakers();
+    }
+}
